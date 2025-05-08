6cfd41f7fc7e89963eb2d2232f6ad1fcccc1d81a
Dashnet Uz <dashnet.uz@gmail.com>
2025-05-08 17:31:30 +0500

    project

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -6,28 +6,23 @@ use yii\web\AssetBundle;
 
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot/template/assets';
-    public $baseUrl = '@web/template/assets';
+    public $basePath = '@webroot/app';
+    public $baseUrl = '@web/app';
 
     public $css = [
-        'css/style.min.css',
-        'libs/owl.carousel/dist/assets/owl.carousel.min.css',
-        'libs/owl.carousel/dist/assets/owl.theme.default.min.css',
-        'libs/aos/dist/aos.css',
-        'libs/simplebar/dist/simplebar.min.css',
-        'libs/bootstrap-icons/font/bootstrap-icons.css',
-        'libs/feather-icons/dist/feather.css',
+        'css/bootstrap.min.css',
+        'css/plugin.css',
+        'css/default.css',
+        'css/styles.css',
+        'icons/font-awesome.min.css',
     ];
 
     public $js = [
-        'libs/jquery/dist/jquery.min.js',
-        'libs/bootstrap/dist/js/bootstrap.bundle.min.js',
-        'libs/simplebar/dist/simplebar.min.js',
-        'libs/aos/dist/aos.js',
-        'libs/owl.carousel/dist/owl.carousel.min.js',
-        'js/app.min.js',
-        'js/app.init.js',
-        'js/theme.js',
+        'js/jquery-3.7.1.min.js',
+        'js/bootstrap.bundle.min.js',
+        'js/custom-nav.js',
+        'js/plugin.js',
+        'js/main.js',
     ];
 
     public $jsOptions = [
