05bc9978b87bee89422ad37a8d32a0733de1a6b3
Dashnet Uz <dashnet.uz@gmail.com>
2025-05-10 11:49:39 +0500

    language

--- common/messages/uz/app.php
+++ common/messages/uz/app.php
@@ -1,56 +1,30 @@
 <?php
-return [
-    'Login' => 'Kirish',
-    'Register' => 'Ro‘yxatdan o‘tish',
-    'Email' => 'Elektron pochta',
-    'Password' => 'Parol',
-    'Go Back to Home' => 'Bosh sahifaga qaytish',
-
-//    login.php
-    'Let\'s get you signed in' => 'Tizimga kirishingizni boshlaymiz',
-    'Sign in with' => 'Tizimga kirish',
-    'Or sign in with email' => 'Yoki email orqali tizimga kiring',
-    'Enter your username' => 'Foydalanuvchi nomingizni kiriting',
-    'Enter your password' => 'Parolingizni kiriting',
-    'Forgot your password?' => 'Parolingizni unutdingizmi?',
-    'Don\'t have an account yet?' => 'Hisobingiz yo‘qmi?',
-    'Sign Up Now' => 'Hozir ro‘yxatdan o‘ting',
-    'Google login muvaffaqiyatsiz. Iltimos qayta urinib ko‘ring.' => 'Google orqali kirish muvaffaqiyatsiz. Qayta urinib ko‘ring.',
-
-//    register.php
-    'Create your account' => 'Hisobingizni yarating',
-    'Choose a username' => 'Foydalanuvchi nomini tanlang',
-    'Enter your email' => 'Email manzilingizni kiriting',
-    'Create a password' => 'Parol yarating',
-    'Already have an account?' => 'Allaqachon hisobingiz bormi?',
-    'Sign In' => 'Tizimga kirish',
-
-//    request-password-reset.php
-    'Forgot Password' => 'Parolni unutdingizmi?',
-    'Reset your password' => 'Parolingizni tiklang',
-    'Send Password Reset Link' => 'Parolni tiklash havolasini yuborish',
-    'Remember your password?' => 'Parolingizni esladingizmi?',
-//    reset-password.php
-    'Reset Password' => 'Parolni tiklash',
-    'Create new password' => 'Yangi parol yaratish',
-    'Enter your new password' => 'Yangi parolingizni kiriting',
-    'Go back to login?' => 'Kirish sahifasiga qaytish?',
-    'Logo-Dark' => 'Logo-dark',
-
-    '{attribute} to‘ldirilishi shart.' => '{attribute} to‘ldirilishi shart.',
-    'Username' => 'Foydalanuvchi nomi',
-    'Email formati noto‘g‘ri.' => 'Email formati noto‘g‘ri.',
-    'Email Address' => 'Email manzili',
-    'Loading' => 'Yuklanmoqda',
-    'Slide' => 'Slayd',
-    'Login side image' => 'Login sahifa rasmi',
-    'Feature Rich 3D Charts' => 'Boy funksiyali 3D grafiklar',
-    'Feature Rich 2D Charts' => 'Boy funksiyali 2D grafiklar',
-    'Feature Rich 1D Charts' => 'Boy funksiyali 1D grafiklar',
-    'Lorem ipsum 3D charts' => '3D grafiklar haqida tasvir',
-    'Lorem ipsum 2D charts' => '2D grafiklar haqida tasvir',
-    'Lorem ipsum 1D charts' => '1D grafiklar haqida tasvir',
-    'Learn More' => 'Batafsil o‘rganish',
-
-
-];
+return array(
+    "Uzbek" => "O'zbekcha",
+    "Russian" => "Ruscha",
+    "English" => "Inglizcha",
+    "Home" => "Asosiy sahifa",
+    "Ariza topshirish" => "Ariza topshirish",
+    "Ta`lim yo`nalishlari" => "Ta`lim yo`nalishlari",
+    "Avval ta'lim darajani tanlang!" => "Avval ta'lim darajani tanlang!",
+    "DTM sertifikati (Agar mavjud bo`lsa)" => "DTM sertifikati (Agar mavjud bo`lsa)",
+    "3x4 rasmingiz (Agar mavjud bo`lsa)" => "3x4 rasmingiz (Agar mavjud bo`lsa)",
+    "DTM sertifikati" => "DTM sertifikati",
+    "Telefon nomeringiz" => "Telefon nomeringiz",
+    "gektar" => "gektar",
+    "Seminar xonalari" => "Seminar xonalari",
+    "dona" => "dona",
+    "Ma’ruza zallari" => "Ma’ruza zallari",
+    "Umumiy bino maydoni" => "Umumiy bino maydoni",
+    "m²" => "m²",
+    "Umumiy maydon" => "Umumiy maydon",
+    "" => "",
+    "" => "",
+    "" => "",
+    "" => "",
+    "" => "",
+    "" => "",
+    "" => "",
+    "" => "",
+)
+?>
