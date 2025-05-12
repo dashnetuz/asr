a5dbcce13ad56d35ebb7001997a592becc30f72d
Dashnet Uz <dashnet.uz@gmail.com>
2025-05-12 15:45:11 +0500

    banner-one-thumb olib tashlandi

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -70,7 +70,7 @@ JS
                                         </div>
                                     </div>
 
-                                    <div class="banner-one-thumb col-xl-6 col-lg-5 pl-60 pl-md-15 pl-xs-15">
+                                    <div class="col-xl-6 col-lg-5 pl-60 pl-md-15 pl-xs-15">
                                         <div class="thumb">
                                             <img style="border-radius: 10%;" decoding="async"
                                                  src="/unieducation/wp-content/uploads/baholash_markazi.jpg"
