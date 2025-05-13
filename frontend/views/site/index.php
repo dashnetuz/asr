ff462828f3ead1199b68f5ba0a175f519d6ecf56
Dashnet Uz <dashnet.uz@gmail.com>
2025-05-13 10:15:55 +0500

    text va rasm joylashuviga test

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -52,40 +52,47 @@ JS
                                                      alt="Image not found">
                                             </div>
 
-                                            <h4 class="wow fadeInUp" data-wow-delay="500ms"data-wow-duration="400ms">
-                                                <?=Yii::t('app', '<strong>Britaniya</strong> Ta`lim Standartlari O`zbekistonda')?>
+                                            <h4 class="wow fadeInUp" data-wow-delay="500ms" data-wow-duration="400ms">
+                                                <?= Yii::t('app', '<strong>Britaniya</strong> Ta`lim Standartlari O`zbekistonda') ?>
                                             </h4>
-                                            <p class="wow fadeInUp" data-wow-delay="500ms"data-wow-duration="400ms">
-                                                <?=Yii::t('app', '<strong>Endi Britaniya universiteti diplomini O`zbekistonda qo`lga kiriting</strong>')?>
-                                                <br>
-                                                <?=Yii::t('app', 'Yangi tashkil etilgan Xalqaro Malakalar va Baholash Markazi (IQAC) orqali jahon darajasidagi ta`lim endi yanada yaqinroq bo`ldi')?>
-                                                <br>
-                                                <?=Yii::t('app', '<strong>Biz haqimizda</strong>')?>
-                                                <br>
-                                                <?=Yii::t('app', 'O`zbekiston Respublikasi Prezident huzuridagi Strategik islohotlar agentligi tashabbusi bilan, Oliy ta`lim, fan va innovatsiyalar vazirligi hamkorligida, O`zbekiston Respublikasi Vazirlar Mahkamasining qaroriga asosan IQAC tashkil etildi. Markazning asosiy maqsadi - xalqaro mezonlarga moslashtirilgan ta`lim dasturlarini mahalliy miqyosda taqdim etish.')?>
-                                                <br>
-                                                <?=Yii::t('app', '<strong>Xalqaro standartlar, mahalliy ahamiyat</strong>')?>
-                                                <br>
-                                                <?=Yii::t('app', 'IQAC tomonidan taklif etilayotgan 3, 4, 5, 6 va 7-darajadagi (professional akademik) malakalar Angliya OFQUAL tomonidan tasdiqlangan RQF (Regulated Qualifications Framework) standartlariga mos ravishda ishlab chiqilgan. Shu bilan birga, ular O`zbekistonning milliy ta`lim tizimi bilan uyg`unlashtirilgan.
 
-                                                IQAC 3-7 darajadagi RQF asosidagi dasturlarni ishlab chiqadi va akkreditatsiyadan o`tkazadi. Ushbu dasturlar quyidagi yetakchi O`zbekiston universitetlarida hamkorlikda amalga oshiriladi:
-- Toshkentdagi INHA Universiteti
-                                                - Toshkent Arxitektura va Qurilish Universiteti
-                                                - Toshkent Davlat Sharqshunoslik Universiteti
+                                            <div class="wow fadeInUp" data-wow-delay="500ms" data-wow-duration="400ms">
 
-Mazkur dasturlar xalqaro miqyosda tan olinadi. Bu Chartered Management Institute (CMI) hamda quyidagi nufuzli Britaniya universitetlari bilan hamkorlikda ta`minlanadi:
-- Gloucestershire Universiteti
-- Hertfordshire Universiteti
-- QA Higher Education
-- Northumbria Universiteti
-- Ulster Universiteti
-- West London Universiteti
-- Central Lancashire Universiteti
-- Wolverhampton Universiteti
-- va boshqalar.
-')?>
-                                                <br>
-                                            </p>
+                                                <p><?= Yii::t('app', '<strong>Endi Britaniya universiteti diplomini O`zbekistonda qo`lga kiriting</strong>') ?></p>
+
+                                                <p><?= Yii::t('app', 'Yangi tashkil etilgan Xalqaro Malakalar va Baholash Markazi (IQAC) orqali jahon darajasidagi ta`lim endi yanada yaqinroq bo`ldi.') ?></p>
+
+                                                <p><strong><?= Yii::t('app', 'Biz haqimizda') ?></strong></p>
+
+                                                <p><?= Yii::t('app', 'O`zbekiston Respublikasi Prezident huzuridagi Strategik islohotlar agentligi tashabbusi bilan, Oliy ta`lim, fan va innovatsiyalar vazirligi hamkorligida, O`zbekiston Respublikasi Vazirlar Mahkamasining qaroriga asosan IQAC tashkil etildi. Markazning asosiy maqsadi - xalqaro mezonlarga moslashtirilgan ta`lim dasturlarini mahalliy miqyosda taqdim etish.') ?></p>
+
+                                                <p><strong><?= Yii::t('app', 'Xalqaro standartlar, mahalliy ahamiyat') ?></strong></p>
+
+                                                <p><?= Yii::t('app', 'IQAC tomonidan taklif etilayotgan 3, 4, 5, 6 va 7-darajadagi (professional akademik) malakalar Angliya OFQUAL tomonidan tasdiqlangan RQF (Regulated Qualifications Framework) standartlariga mos ravishda ishlab chiqilgan. Shu bilan birga, ular O`zbekistonning milliy ta`lim tizimi bilan uyg`unlashtirilgan.') ?></p>
+
+                                                <p><?= Yii::t('app', 'IQAC 3-7 darajadagi RQF asosidagi dasturlarni ishlab chiqadi va akkreditatsiyadan o`tkazadi. Ushbu dasturlar quyidagi yetakchi O`zbekiston universitetlarida hamkorlikda amalga oshiriladi:') ?></p>
+
+                                                <ul>
+                                                    <li><?= Yii::t('app', 'Toshkentdagi INHA Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'Toshkent Arxitektura va Qurilish Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'Toshkent Davlat Sharqshunoslik Universiteti') ?></li>
+                                                </ul>
+
+                                                <p><?= Yii::t('app', 'Mazkur dasturlar xalqaro miqyosda tan olinadi. Bu Chartered Management Institute (CMI) hamda quyidagi nufuzli Britaniya universitetlari bilan hamkorlikda ta`minlanadi:') ?></p>
+
+                                                <ul>
+                                                    <li><?= Yii::t('app', 'Gloucestershire Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'Hertfordshire Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'QA Higher Education') ?></li>
+                                                    <li><?= Yii::t('app', 'Northumbria Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'Ulster Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'West London Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'Central Lancashire Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'Wolverhampton Universiteti') ?></li>
+                                                    <li><?= Yii::t('app', 'va boshqalar.') ?></li>
+                                                </ul>
+                                            </div>
+
 
                                             <div class="button mt-40 wow fadeInUp" data-wow-delay="1200ms"
                                                  data-wow-duration="400ms">
