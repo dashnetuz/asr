e25aeb0560faac056f05b701ca1a48e3e1bc4c84
Dashnet Uz <dashnet.uz@gmail.com>
2025-06-04 14:28:05 +0500

    contact form

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -61,8 +61,8 @@ return [
             'showScriptName' => false,
             'enableDefaultLanguageUrlCode' => true,
             'rules' => [
-                '/<url:[a-zA-Z0-9-]+>/'=>'site/page',
                 'contact' => 'site/contact',
+                '/<url:[a-zA-Z0-9-]+>/'=>'site/page',
                 'page' => 'site/page',
                 'dashboard/<type:(category|course|lesson|part)>/<action:(index|create|update|delete)>' => 'dashboard/<type><action>',
                 # role:
