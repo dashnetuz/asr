84ed9e82648e09a4cc3319c3b3eff410ce1f33f7
ADSh <@dashnetuz>
2025-06-23 11:45:57 +0500

    email yuborish

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -12,35 +12,10 @@ return [
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@common/mail',
-            // send all mails to a file by default.
             'useFileTransport' => false,
-            // You have to set
-            //
-            // 'useFileTransport' => false,
-            //
-            // and configure a transport for the mailer to send real emails.
             'transport' => [
-                'dsn' => 'smtp://davlatbek.abduvoxidov97@gmail.com:D*Z9799d@smtp.gmail.com:587?encryption=tls',
+                'dsn' => 'smtp://davlatbek.abduvoxidov97@gmail.com:pmlfucottwbxgybb@smtp.gmail.com:587?encryption=tls',
             ],
-            //
-            // SMTP server example:
-            //    'transport' => [
-            //        'scheme' => 'smtps',
-            //        'host' => '',
-            //        'username' => '',
-            //        'password' => '',
-            //        'port' => 465,
-            //        'dsn' => 'native://default',
-            //    ],
-            //
-            // DSN example:
-            //    'transport' => [
-            //        'dsn' => 'smtp://user:pass@smtp.example.com:25',
-            //    ],
-            //
-            // See: https://symfony.com/doc/current/mailer.html#using-built-in-transports
-            // Or if you use a 3rd party service, see:
-            // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
         ],
     ],
 ];
